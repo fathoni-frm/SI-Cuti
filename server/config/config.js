ad86d07488d1545355b686beb693398c415fd4f4
fathoni-frm <fathoni.frm@gmail.com>
2025-11-14 09:39:58 +0800

    menambahkan konfigurasi untuk lingkungan produksi di config.js

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   }
 };
